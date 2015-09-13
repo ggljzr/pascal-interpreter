6cd053f2925190b4dc9f3f0d73c3130a74db905d
Gogo Lejzr <ggljzr@localhost.localdomain>
2015-09-13 17:32:31 +0200

    added simple division, refactoring

--- rust-calc/src/main.rs
+++ rust-calc/src/main.rs
@@ -1,18 +1,23 @@
 use std::io;
 
+//if struct wants to use clone method
+//all its elements must implement this method
 #[derive(PartialEq, Clone)]
 enum Token_type{
     Integer,
     Plus,
     Minus,
+    Multi,
+    Div,
     EoF,
     Empty
 }
 
+//we going to need a copy of instance later
 #[derive(Clone)]
 struct Token {
     token_type : Token_type,
-    token_value : i32,
+    token_value : i64,
 }
 
 struct Interpreter {
@@ -26,6 +31,7 @@ struct Interpreter {
 impl Interpreter {
     fn new(input_text: String) -> Interpreter {
 
+        //transfer input string into vector of chars
         let chars : Vec<char> = input_text.chars().collect();
         let c = chars[0];
         let empty_token = Token{ token_type: Token_type::Empty, token_value: 0};
@@ -55,7 +61,7 @@ impl Interpreter {
         }
     }
 
-    fn integer(&mut self) -> i32{
+    fn integer(&mut self) -> i64{
         let mut result = String::new();
 
         while !self.input_end && self.current_char.is_digit(10){
@@ -63,7 +69,7 @@ impl Interpreter {
             self.advance();
         }
         
-        let result: i32 = result.parse()
+        let result: i64 = result.parse()
             .ok()
             .expect("not a number");
 
@@ -91,6 +97,19 @@ impl Interpreter {
                              token_value: -1}
             }
 
+            if self.current_char == '*'
+            {
+                self.advance();
+                return Token{token_type: Token_type::Multi,
+                             token_value: 2}
+            }
+
+            if self.current_char == '/'{
+                self.advance();
+                return Token{token_type: Token_type::Div,
+                             token_value: -2}
+            }
+
             self.error();
         }
 
@@ -106,30 +125,35 @@ impl Interpreter {
         }
     }
 
-    fn expr(&mut self) -> i32{
+    fn expr(&mut self) -> i64{
         self.current_token = self.get_next_token();
 
-        let left = self.current_token.clone();
-        self.eat(Token_type::Integer);
+        let left = self.current_token.clone(); //we need to make a copy of token before
+        self.eat(Token_type::Integer); //rewriting
 
         
         let op = self.current_token.clone();
-        if op.token_type == Token_type::Plus {
-            self.eat(Token_type::Plus);
+        
+        match op.token_type {
+            Token_type::Plus => self.eat(Token_type::Plus),
+            Token_type::Minus => self.eat(Token_type::Minus),
+            Token_type::Multi => self.eat(Token_type::Multi),
+            Token_type::Div => self.eat(Token_type::Div),
+            _ => self.error()
         }
-        else{
-            self.eat(Token_type::Minus);
-        }
+
 
         let right = self.current_token.clone();
         self.eat(Token_type::Integer);
-
-        if op.token_type == Token_type::Plus {
-            left.token_value + right.token_value
+        
+        match op.token_type {
+            Token_type::Plus => left.token_value + right.token_value,
+            Token_type::Minus => left.token_value - right.token_value,
+            Token_type::Multi => left.token_value * right.token_value,
+            Token_type::Div => left.token_value / right.token_value,
+            _ => panic!("this should not happen")
         }
-        else {
-            left.token_value - right.token_value
-        }
+    
     }
 }
 
